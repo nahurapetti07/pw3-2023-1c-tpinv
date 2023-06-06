d883f6a4691e821d7248af039c067fac6b8c7761
Manuel Oyaregui <manu.oyaregui@gmail.com>
2023-06-06 17:22:57 -0300

    agrego la entrega

---
+++ grupo-1/GrupoAzureWebIII/Migrations/20230605233841_creoNuevaMigra.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrupoAzureWebIII.Migrations
+{
+    /// <inheritdoc />
+    public partial class creoNuevaMigra : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mensaje",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    mensaje = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    medioEnvio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    remitente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    destinatario = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mensaje", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mensaje");
+        }
+    }
+}
---
+++ grupo-1/GrupoAzureWebIII/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrupoAzureWebIII.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
